23a45ca0c7b33c9fc562beae2e6930f47e48dded
KenV99 <KenV99@users.noreply.github.com>
2014-11-14 07:10:53 -0500

    Commit before gh-pages update

--- docs/docs_to_gh-pages.sh
+++ docs/docs_to_gh-pages.sh
@@ -2,6 +2,7 @@
 rm -rf "C:\Temp\html"
 mkdir "C:\Temp\html"
 cp -rf ./_build/html/* "C:\Temp\html"
-git commit -a
-git status
+git commit -a -m "Commit before gh-pages update"
+git checkout gh-pages
+ls
 exit
