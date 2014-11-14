513cd915e7852500ac624e2d47ce5babc204cc89
KenV99 <KenV99@users.noreply.github.com>
2014-11-14 07:24:58 -0500

    Commit before gh-pages update

--- docs/docs_to_gh-pages.sh
+++ docs/docs_to_gh-pages.sh
@@ -2,7 +2,10 @@
 rm -rf "C:\Temp\html"
 mkdir "C:\Temp\html"
 cp -rf ./_build/html/* "C:\Temp\html"
+cp .nojekyll "C:\Temp\html"
+cp .gitignore "C:\Temp\html"
 git commit -a -m "Commit before gh-pages update"
 git checkout gh-pages
+rm -rf *
 ls
 exit
